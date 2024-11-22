c6a4f6a75e5f5671ae74876b36697f84f7a6913a
Frank Kang <holycow001@gmail.com>
2024-11-22 14:32:28 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,12 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+const firstElement = languages[0];
+const thirdElement = languages[2];
+
+console.log(`the first element of the array is ${firstElement}`);
+console.log(`the third element of the array is ${thirdElement}`);
+
+const length = languages.length;
+console.log(`the length of the array is ${length}`);
+console.log(`the last element of the array is ${languages[length - 1]}`);
